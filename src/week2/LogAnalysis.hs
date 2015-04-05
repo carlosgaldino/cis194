4e908974d7c8457eb2fecb4ff2a13800cbb6e72b
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-05 16:33:42 -0300

    Finish week 2.

--- src/week2/LogAnalysis.hs
+++ src/week2/LogAnalysis.hs
@@ -13,3 +13,37 @@ parseMessage = parseMessage' . words
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert nlog Leaf = Node Leaf nlog Leaf
+insert nlog (Node left log right)
+  | getTimeStamp nlog > getTimeStamp log  = Node left log (insert nlog right)
+  | getTimeStamp nlog <= getTimeStamp log = Node (insert nlog left) log right
+
+getTimeStamp :: LogMessage -> TimeStamp
+getTimeStamp (LogMessage _ ts _) = ts
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left l right) = inOrder left ++ [l] ++ inOrder right
+
+isError :: LogMessage -> Bool
+isError (LogMessage (Error _) _ _) = True
+isError _                          = False
+
+isSevere :: LogMessage -> Bool
+isSevere (LogMessage (Error i) _ _)
+  | i >= 50   = True
+  | otherwise = False
+isSevere _    = False
+
+getMessage :: LogMessage -> String
+getMessage (LogMessage _ _ msg) = msg
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong ls = map getMessage errors
+  where errors = inOrder . build $ filter (\x -> isSevere x && isError x) ls
