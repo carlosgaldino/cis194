d68957d381ae46e77ff5d9cb60476fcaae28f956
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-07 09:11:12 -0300

    Initial work on week 4 exercises.

---
+++ src/week4/Week4.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Week4 where
+
+fun1 :: [Integer] -> Integer
+fun1 = product . map (subtract 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 = sum . filter even . takeWhile (/= 1) . iterate f
+  where f n = if even n then n `div` 2 else 3 * n + 1
