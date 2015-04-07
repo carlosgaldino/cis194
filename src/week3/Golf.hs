0fdbe051f6f97d1cf00a9886747a299bee22bdb0
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-06 23:11:46 -0300

    Finish week 3 exercises.

--- src/week3/Golf.hs
+++ src/week3/Golf.hs
@@ -1,15 +1,32 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Golf where
+
+import Data.List (transpose, intercalate)
 
 skips :: [a] -> [[a]]
 skips xs = map (\n -> skip n xs) idxs
-  where skip n xs = map snd . filter (\(i, _) -> i `rem` n == 0) $ zip idxs xs
+  where skip n ys = map snd . filter (\(i, _) -> i `rem` n == 0) $ zip idxs ys
         idxs = [1..length xs]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima xs = map (\(_, x, _) -> x) . filter (\(x, y, z) -> y > x && y > z) $ zip3 xs (tail xs) (drop 2 xs)
 
-histogram :: [Integer] -> String
-histogram = undefined
+histogram :: [Int] -> String
+histogram xs = intercalate "\n" $ hist xs ++ [base]
+  where max  = maximum $ frequences xs
+        hist = transpose . map (complete max) . points . frequences
 
-frequences :: [Integer] -> [(Int, Integer)]
-frequences xs = [(length fs, x) | x <- [0..9], let fs = filter (== x) xs]
+frequences :: [Int] -> [Int]
+frequences xs = [freq | x <- [0..9], let freq = length $ filter (== x) xs]
+
+points :: [Int] -> [String]
+points = map (\x -> concat $ replicate x "*")
+
+complete :: Int -> String -> String
+complete n xs = concat (replicate (n - length xs) " ") ++ xs
+
+base :: String
+base = concat $ delimiter ++ ["\n"] ++ numbers
+  where delimiter = replicate 10 "="
+        numbers   = map show ([0..9] :: [Int])
