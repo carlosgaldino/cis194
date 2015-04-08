fce41a96b121d9d067335fa78e9b63d1f06a2ede
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-08 00:17:39 -0300

    Add more exercises from week 4.

--- src/week4/Week4.hs
+++ src/week4/Week4.hs
@@ -8,3 +8,14 @@ fun1 = product . map (subtract 2) . filter even
 fun2 :: Integer -> Integer
 fun2 = sum . filter even . takeWhile (/= 1) . iterate f
   where f n = if even n then n `div` 2 else 3 * n + 1
+
+xor :: [Bool] -> Bool
+xor = foldl (\x y -> not (x == y)) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> f x : y) []
+
+-- http://stackoverflow.com/questions/6172004/writing-foldl-using-foldr
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f z xs = foldr step id xs z
+  where step x g a = g (f a x)
