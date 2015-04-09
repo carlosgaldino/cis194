fdd321391f9b8a544f2442520ecc496632624bdc
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-09 01:29:12 -0300

    Add exercise 5 from week 5.

---
+++ src/week5/Exercise5.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Exercise5 where
+
+import StackVM
+import Parser
+
+class Expr a where
+  lit :: Integer -> a
+  mul :: a -> a -> a
+  add :: a -> a -> a
+
+instance Expr Program where
+  lit x = [PushI x]
+  mul x y = x ++ y ++ [Mul]
+  add x y = x ++ y ++ [Add]
+
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
