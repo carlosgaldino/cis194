acc774687ee423b3eb564bd39d3158bc865f406c
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-11 15:54:23 -0300

    Finish week 6.

--- src/week6/Fibonacci.hs
+++ src/week6/Fibonacci.hs
@@ -14,7 +14,7 @@ fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
 data Stream a = Cons a (Stream a)
 
 instance Show a => Show (Stream a) where
-  show s = show $ take 20 (streamToList s)
+  show s = show $ take 100 (streamToList s)
 
 streamToList :: Stream a -> [a]
 streamToList (Cons a s) = a : streamToList s
@@ -30,3 +30,15 @@ streamFromSeed f s = Cons s (streamFromSeed f (f s))
 
 nats :: Stream Integer
 nats = streamFromSeed (+1) 0
+
+ruler = interleaveStreams (streamRepeat 0)
+        (interleaveStreams (streamRepeat 1)
+        (interleaveStreams (streamRepeat 2)
+        (interleaveStreams (streamRepeat 3)
+        (interleaveStreams (streamRepeat 4) (streamFromSeed (+1) 5)))))
+
+zipStreams :: Stream a -> Stream b -> [(a, b)]
+zipStreams s t = zip (streamToList s) (streamToList t)
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x s) (Cons y t) = Cons x (Cons y (interleaveStreams s t))
