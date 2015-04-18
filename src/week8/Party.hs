e93d8aa4d38dc7b34152c8324d11fb748dbe2749
Carlos Galdino <carloshsgaldino@gmail.com>
2015-04-17 22:03:16 -0300

    Finish week 8.

--- src/week8/Party.hs
+++ src/week8/Party.hs
@@ -5,6 +5,7 @@ module Party where
 import Employee
 import Data.Monoid
 import Data.Tree
+import Data.List (sort)
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL es fun) = GL (e : es) (fun + empFun e)
@@ -19,6 +20,25 @@ moreFun = max
 
 treeFold :: (b -> a -> b) -> b -> Tree a -> b
 treeFold f z t = foldl f z (nodeList t)
+  where nodeList (Node a as) = a : concatMap nodeList as
 
-nodeList :: Tree a -> [a]
-nodeList (Node a as) = a : concatMap nodeList as
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e gs = (maximum (map fst maxis), maximum (map snd maxis))
+  where maxis = map (\(x, y) -> (moreFun (glCons e x) (glCons e y), moreFun x y)) gs
+
+maxFun :: Tree Employee -> GuestList
+maxFun t = uncurry moreFun $ treeFold (\gs e -> nextLevel e [gs]) emptyGL t
+  where emptyGL = (mempty, mempty)
+
+printFun :: GuestList -> IO ()
+printFun (GL _ f) = putStrLn ("Total fun: " ++ show f)
+
+printGuests :: GuestList -> IO ()
+printGuests (GL gs _) = mapM_ print $ sort (map empName gs)
+
+main :: IO ()
+main = do
+  content <- readFile "company.txt"
+  let guestList = maxFun (read content :: Tree Employee)
+  printFun guestList
+  printGuests guestList
